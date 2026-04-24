10a0a77aa9846b4a74b89fc49adcfb3b19b556eb
tentangsaya78 <tentangsaya78@gmail.com>
2026-04-24 10:02:07 +0700

    rule

--- theme/template-parts/home/rule.php
+++ theme/template-parts/home/rule.php
@@ -54,7 +54,7 @@ $rules = new WP_Query($args);
                             <?php endif; ?>
 
                         </div>
-                        <div class="my-paragraph text-xl flex flex-col gap-5">
+                        <div class="my-paragraph text-xl flex flex-col gap-5 w-full wrap-break-word">
                             <h3 class="text-3xl lg:text-5xl mb-5 font-medium">
                                 <?php echo wp_kses_post($title); ?>
                                 <?php if ($subtitle) : ?>
