d26cf3d1a1f17a67f1d96d7d30acb8801c87fad3
tentangsaya78 <tentangsaya78@gmail.com>
2026-05-15 10:05:06 +0700

    list

--- theme/single-rule.php
+++ theme/single-rule.php
@@ -29,18 +29,25 @@ $rules = new WP_Query($args);
     </div>
     <div class="container pb-20">
         <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start">
-       <!--      <div class="lg:col-span-3 sticky top-40 self-start">
-                <?php // if ($rules->have_posts()): ?>
+            <!--      <div class="lg:col-span-3 sticky top-40 self-start">
+                <?php // if ($rules->have_posts()): 
+                ?>
                     <ul class="rules-list ">
-                        <?php // while ($rules->have_posts()): $rules->the_post(); ?>
-                            <li class="mb-3 <?php /* current rule */ // echo get_the_ID() === get_queried_object_id() ? 'text-primary font-medium' : '' ?>">
-                                <a href="<?php // echo get_the_permalink(); ?>" class="text-lg hover:text-accent duration-200">
-                                    <?php // echo get_the_title(); ?>
+                        <?php // while ($rules->have_posts()): $rules->the_post(); 
+                        ?>
+                            <li class="mb-3 <?php /* current rule */ // echo get_the_ID() === get_queried_object_id() ? 'text-primary font-medium' : '' 
+                                            ?>">
+                                <a href="<?php // echo get_the_permalink(); 
+                                            ?>" class="text-lg hover:text-accent duration-200">
+                                    <?php // echo get_the_title(); 
+                                    ?>
                                 </a>
                             </li>
-                        <?php // endwhile; ?>
+                        <?php // endwhile; 
+                        ?>
                     </ul>
-                <?php // endif; ?>
+                <?php // endif; 
+                ?>
             </div> -->
             <div class="lg:col-span-9 rule-content">
                 <?php while (have_posts()) : the_post(); ?>
@@ -48,24 +55,26 @@ $rules = new WP_Query($args);
                 <?php endwhile;
 
                 if (is_singular('rule')): ?>
-                 
-                    <div class="pb-5 pt-5 border-t border-gray-300 mt-10">
-                        
-                        <?php
-                        the_post_navigation(
-                            array(
-                                'next_text' => '<span aria-hidden="true" class="text-sm italic text-muted">' . __('Next ', 'ssdc') . '</span> ' .
-                                    '<span class="sr-only">' . __('Next:', 'ssdc') . '</span> <br/>' .
-                                    '<span class="font-medium">%title</span>',
-                                    
-                                'prev_text' => '<span aria-hidden="true" class="text-sm italic text-muted">' . __('Previous ', 'ssdc') . '</span> ' .
-                                    '<span class="sr-only ">' . __('Previous:', 'ssdc') . '</span> <br/>' .
-                                    '<span class="font-medium">%title</span>',
-                            )
-                        ); ?>
-                    </div>
+                    <div class="rule-footer">
 
-                     <a href="/#rule" class=" mb-5 mt-5 block hover:text-accent duration-200">Back to Menu <i class="bi bi-arrow-right"></i></a>
+                        <div class="pb-5 pt-5 border-t border-gray-300 mt-10">
+                            <?php
+                            the_post_navigation(
+                                array(
+                                    'next_text' => '<span aria-hidden="true" class="text-sm italic text-muted">' . __('Next ', 'ssdc') . '</span> ' .
+                                        '<span class="sr-only">' . __('Next:', 'ssdc') . '</span> <br/>' .
+                                        '<span class="font-medium">%title</span>',
+
+                                    'prev_text' => '<span aria-hidden="true" class="text-sm italic text-muted">' . __('Previous ', 'ssdc') . '</span> ' .
+                                        '<span class="sr-only ">' . __('Previous:', 'ssdc') . '</span> <br/>' .
+                                        '<span class="font-medium">%title</span>',
+                                )
+                            ); ?>
+                        </div>
+
+                        <a href="/#rule" class=" mb-5 mt-5 block hover:text-accent duration-200">Back to Menu <i class="bi bi-arrow-right"></i></a>
+
+                    </div>
                 <?php endif; ?>
             </div>
         </div>
@@ -76,14 +85,16 @@ $rules = new WP_Query($args);
 get_footer();
 ?>
 <style>
-    .content p, .content ul li , .content ol li {
+    .content p,
+    .content ul li,
+    .content ol li {
         margin-bottom: 1rem;
     }
+
     .rule-header {
         background-image: url('<?php echo get_template_directory_uri(); ?>/assets/grid-2.png');
         background-position: center;
     }
-    .rules-list{
 
-    }
+    .rules-list {}
 </style>
