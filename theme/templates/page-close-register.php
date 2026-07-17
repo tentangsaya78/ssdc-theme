ce39c00401a4820d82edb671e66ac46a39fcf74b
tentangsaya78 <tentangsaya78@gmail.com>
2026-07-17 08:11:48 +0700

    close regirter page

---
+++ theme/templates/page-close-register.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: Close Register Page */
+get_header();
+?>
+<section class="bg-primary text-white/80">
+    <div class="container flex justify-center items-center min-h-[calc(100vh-100px)]">
+        <div class="max-w-3xl text-center p-6 lg:p-10 mx-auto">
+            <p>Ok but should not be page not found. Should say registration has closed...please use your <a href="<?= wp_login_url(); ?>" class="underline hover:text-secondary"> to login </a> to submit via our portal</p>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
